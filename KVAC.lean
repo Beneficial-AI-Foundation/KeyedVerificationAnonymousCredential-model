1c9616c8788ce4964092ee38610be821d2ef4cbf
semar <semaraugusto@gmail.com>
2026-07-02 18:44:59 -0300

    fixes merge conflict

--- KVAC.lean
+++ KVAC.lean
@@ -3,8 +3,5 @@ import KVAC.Core.Hash
 import KVAC.Core.ZKProof
 import KVAC.Core.AlgebraicMAC
 import KVAC.Core.AlgebraicMAC.Security
-<<<<<<< microCMZ-construction
 import KVAC.Schemes.MicroCMZ.Construction
-=======
 import KVAC.Preliminaries.Assumptions
->>>>>>> main
