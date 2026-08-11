d2b75f5f4117f6764c98fff052875bdbd731d3ab
semaraugusto <semaraugusto@gmail.com>
2026-08-10 21:52:07 -0300

    cleans up proofs a bit and tries to fix CI on formalization progress

--- KVAC.lean
+++ KVAC.lean
@@ -8,9 +8,9 @@ import KVAC.Core.NIZKP.Security
 import KVAC.Core.NIZKP.Extraction
 import KVAC.Schemes.MicroCMZ.AGMPolynomial
 import KVAC.Schemes.MicroCMZ.Relations
-import KVAC.Schemes.MicroCMZ.AGMReduction
 import KVAC.Schemes.MicroCMZ.Construction
 import KVAC.Schemes.MicroCMZ.AlgebraicMAC
 import KVAC.Schemes.MicroCMZ.SignMask
+import KVAC.Schemes.MicroCMZ.AGMReduction
 import KVAC.Preliminaries.Assumptions
 import KVAC.Framework
